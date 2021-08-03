27ae953b79cf49b07e11cc2fb3bb1e28401564a0
Lucio Flavio Boaventura <lucio.finan@gmail.com>
2021-08-03 12:15:00 -0300

    ajustes main.js

--- site/src/main.js
+++ site/src/main.js
@@ -7,15 +7,10 @@ import axios from "axios";
 import Vuex from 'vuex';
 Vue.use(Vuex);
 
-// let slug = function (_str) {
-//   return _str.replace(/ /gi, "-").toLowerCase();
-// }
-
 Vue.config.productionTip = false;
 Vue.prototype.$http = axios;
 Vue.prototype.$urlAPI = `http://127.0.0.1:8000/api/`;
 Vue.prototype.$urlViaCep = `https://viacep.com.br/ws/`;
-// Vue.prototype.$slug = slug;
 
 var store = {
   state: {
@@ -29,35 +24,13 @@ var store = {
     getToken: state => {
       return state.user.token;
     },
-    // getTimeline: state => {
-    //   return state.timeline;
-    // },
   },
   mutations: {
     setUser(state, n) {
       state.user = n;
     },
-    // setTimeline(state, n) {
-    //   state.timeline = n;
-    // },
-    // setUpdateTimeline(state, n) {
-    //   for (let item of n) {
-    //     state.timeline.includes(item) ? '' : state.timeline.push(item);
-    //   }
-    // },
   },
 }
-
-// Vue.directive('scroll', {
-//   inserted: function (el, binding) {
-//     let f = function (evt) {
-//       if (binding.value(evt, el)) {
-//         window.removeEventListener('scroll', f)
-//       }
-//     }
-//     window.addEventListener('scroll', f)
-//   }
-// })
 
 
 /* eslint-disable no-new */
